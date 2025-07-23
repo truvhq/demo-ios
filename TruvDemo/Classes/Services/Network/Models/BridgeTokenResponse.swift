9bf9d22e079a34aa3fb82cfd51f120dce365ed22
Aleksei Veselovskii <veselovskiyAI@gmail.com>
2025-07-23 13:41:51 -0400

    Fix bug with empty client_name (#11)

--- TruvDemo/Classes/Services/Network/Models/BridgeTokenResponse.swift
+++ TruvDemo/Classes/Services/Network/Models/BridgeTokenResponse.swift
@@ -11,7 +11,7 @@ struct BridgeTokenResponse: Decodable {
 
     let bridge_token: String
     let company_mapping_id: String?
-    let client_name: String
+    let client_name: String?
     let product_type: String
     let allowed_products: [String]
 
