0d391231efb907e6ba4e053c440a75d60896e84c
Aleksei Veselovskii <veselovskiyai@gmail.com>
2026-07-14 20:04:46 -0400

    Fix userId cache

--- TruvDemo/Classes/Core/AppState.swift
+++ TruvDemo/Classes/Core/AppState.swift
@@ -12,9 +12,29 @@ final class AppState {
     private init() {}
 
     var product = Product()
-    var userId: String? = nil
     var settings: Settings {
         KeychainManager().retrieveSettings() ?? Settings()
+    }
+
+    var userId: String? {
+        get {
+            guard let user = user, user.contextKey == userContextKey else { return nil }
+            return user.id
+        }
+        set {
+            user = newValue.map { (id: $0, contextKey: userContextKey) }
+        }
+    }
+
+    private var user: (id: String, contextKey: String)?
+
+    private var userContextKey: String {
+        let settings = self.settings
+        return [
+            settings.stand.apiUrl,
+            settings.clientId.value ?? "",
+            settings.keyForSelectedEnvironment ?? ""
+        ].joined(separator: "|")
     }
 
 }
